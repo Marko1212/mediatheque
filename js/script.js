6779669d537360026d90f28ddf93b416e67f3b1c
Marko1212 <shihonage7@gmail.com>
2020-05-12 14:29:54 +0200

    change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+
 function testClick() {
-    document.write("Bonjour à tous!");
+    const element = document.getElementById("name");
+    element.value = "Bonjour à tous!";
 }
+
+
+
+var elements = document.getElementsByTagName("p");
+var length = elements.length;
+console.log(length);
+for (var i = 0; i < length; i++) {
+    elements[i].style.color = "red";
+    console.log(elements[i].innerHTML);
+}
+var myLink = document.getElementById("link");
+var myButton = document.getElementById("myButton");
+myButton.addEventListener("click", function (event) {
+    alert(myLink.getAttribute("href"));
+});
